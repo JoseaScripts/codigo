8c99fab0546598137e67f62e5f20dc2c05692f45
JoseaScripts <32377523+JoseaScripts@users.noreply.github.com>
2020-04-14 17:47:15 -0400

    Update and rename proxyurl.js to proxy-url.html

--- proxyurl.js
+++
@@ -1,3 +0,0 @@
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const enlace = "https://www.elmundo.es/servicios/noticias/scroll/comentarios/comunidad/listar.html?noticia=1705996&version=v2"
-        const archivo = proxyurl + enlace
